b98415f4842f35aa7c9d56df41eca30ffedc501b
vaibhavvpro1 <146124931+vaibhavvpro1@users.noreply.github.com>
2023-09-27 22:01:56 +0530

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -6,7 +6,7 @@ import pandas as pd
 st.sidebar.title("Smartbyte Computer Education")
 st.sidebar.text_input("Mail address")
 st.sidebar.text_input("Password")
-st.sidebar.button('Click here')
+st.sidebar.button('Submit')
 st.sidebar.radio("Your age between",["5-10","11-15","16-20","21-25"])
 st.title("Bar chart")
 data=pd.DataFrame(np.random.randn(50,2),columns=["x","y"])
@@ -18,5 +18,5 @@ st.area_chart(data)
 st.sidebar.file_uploader("Upload your file/folder")
 st.chat_input("Type here")
 st.status("Working")
-
+st.header("Vaibhav borse")
 
